75ce091d7ab18ef1830bf41221c48db3d8b2f626
Nonannet <mail001@ntalk.de>
2025-06-06 11:59:08 +0000

    Deploying to gh-pages from @ DLR-Institute-of-Future-Fuels/gaspype@71da33d9ad49887a5437d60a0f21253e684770de 🚀

---
+++ _downloads/7767b5818475db91692705cc5bc62349/sulfur_oxygen_equalibrium.py
@@ -0,0 +1,44 @@
+#  # Sulfur Oxygen Equilibrium
+#  
+#  This example shows equilibrium calculations for sulfur/oxygen mixtures.
+
+import gaspype as gp
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#  List possible sulfur/oxygen species:
+
+gp.species(element_names = 'S, O')
+
+
+#  Or more specific by using regular expressions:
+
+gp.species('S?[2-3]?O?[2-5]?', use_regex=True)
+
+
+#  Calculation of the molar equilibrium fractions for sulfur and oxygen depending on the oxygen to sulfur ratio:
+
+fs = gp.fluid_system(['S2', 'S2O', 'SO2', 'SO3', 'O2'])
+
+oxygen_ratio = np.linspace(0.5, 3, num=128)
+el = gp.elements({'S': 1}, fs) + oxygen_ratio * gp.elements({'O': 1}, fs)
+
+composition = gp.equilibrium(el, 800+273.15, 1e4)
+
+plt.plot(oxygen_ratio, composition.get_x())
+plt.legend(composition.species)
+
+
+#  Calculation of the molar equilibrium fractions for sulfur and oxygen depending on temperature in °C:
+
+fs = gp.fluid_system(['S2', 'S2O', 'SO2', 'SO3', 'O2'])
+
+el = gp.elements({'S': 1, 'O':2.5}, fs)
+
+t_range = np.linspace(500, 1300, num=32)
+composition = gp.equilibrium(el, t_range+273.15, 1e4)
+
+plt.plot(t_range, composition.get_x())
+plt.legend(composition.species)
+
